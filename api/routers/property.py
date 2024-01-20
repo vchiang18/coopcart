35d35c43420558912855392ed362f93f0149c2fe
sherryanne <sherryanneshen@gmail.com>
2024-01-19 19:44:57 -0800

    issue 18 Property Finished

---
+++ api/routers/property.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from queries.property import PropertyIn, PropertyOut, PropertyQueries, Error
+from typing import Union
+
+router = APIRouter()
+
+@router.post("/property")
+def create_property(property: PropertyIn, repo: PropertyQueries = Depends()):
+	repo.create(property)
+	return property
+
+@router.get("/property/{property_id}", response_model= Union[PropertyOut, Error])
+def get_property(property_id: int, repo: PropertyQueries = Depends()):
+    property_data = repo.get(property_id)
+    return property_data
+
+@router.put("/property/{property_id}", response_model= Union[PropertyOut, Error])
+def update_property(property_id: int, property: PropertyIn, repo: PropertyQueries = Depends()):
+    update_property = repo.update(property_id, property)
+    return update_property
