499fb8f5af779a05f0ad731c2891089cd454e225
Victoria Chiang <vchiang18@gmail.com>
2024-02-01 11:56:14 -0800

    signup property page

--- ghi/src/views/SignupProperty.js
+++ ghi/src/views/SignupProperty.js
@@ -1,10 +1,60 @@
-import React , { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useAuthContext } from "@galvanize-inc/jwtdown-for-react";
 
-function SignupProperty(){
-    const [isKM , setIsKM ] = useState([]);
-    const [properties, setProperties ] = useState([]);
+function SignupProperty() {
+  const [isKM, setIsKM] = useState("");
+  const { token } = useAuthContext();
+  //   const [properties, setProperties] = useState([]);
+  //   const [property, setProperty] = useState([]);
 
-    const getProperties = async() => {
-        const url =
-    }
+    const handleIsKM = async () => {
+
+
+      const url = `http://localhost:8000/user`;
+      const user = {};
+      const fetchConfig = {
+        method: "PUT",
+        body: JSON.stringify({user}),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      try {
+          const response = await fetch(url, fetchConfig);
+          if (response.ok) {
+
+          }
+      }
+    };
+
+  //     const getProperties = async () => {
+  //       const url = "http://localhost:8000/properties";
+  //       try {
+  //         const response = await fetch(url);
+  //         if (response.ok) {
+  //           const data = await response.json();
+  //           setProperties(data.properties);
+  //         }
+  //       } catch (err) {
+  //         console.error(err);
+  //       }
+  //     };
+
+  //   useEffect(() => {
+  //     getProperties();
+  //   }, []);
+
+  //   if (properties === undefined) {
+  //     return null;
+  //   }
+
+  // onSubmit - should add isKM to user (edit) / property_id to user (edit)
+
+  return (
+    <>
+      <input type="radio"/>
+      <input type="radio"/>
+    </>
+  );
 }
