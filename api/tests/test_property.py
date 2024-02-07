70786d3e56b868e9bcde240ee8ae0411df4f6ad5
Victoria Chiang <vchiang18@gmail.com>
2024-02-07 09:44:02 -0800

    linting cleanup

--- api/tests/test_property.py
+++ api/tests/test_property.py
@@ -178,24 +178,3 @@ def test_update_property():
         "created_at": "2024-02-01T00:00:00",
         "property_picture_url": "example.url"
     }
-
-
-def test_get_all_property():
-    app.dependency_overrides[PropertyQueries] = GetAllPropertiesQueries
-    response = client.get("/properties")
-    app.dependency_overrides = {}
-    assert response.status_code == 200
-    expected_response = [{
-        "property_id": 1,
-        "property_name": "Dream",
-        "street": "LA",
-        "city": "Pasadena",
-        "zip": "90004",
-        "state": "CA",
-        "total_members": 20,
-        "food_fee": 200.0,
-        "created_at": "2024-02-01T00:00:00",
-        "property_picture_url": "example.url"
-    }]
-    assert response.json() == expected_response
-    app.dependency_overrides = {}
